1fc37d0a600f8b6d7a79d83ca7d13c4b78f90000
Mrsuscatee <167387022+Mrsuscatee@users.noreply.github.com>
2024-04-18 11:57:41 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,415 @@
+namespace SpriteKind {
+    export const reload = SpriteKind.create()
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . f f f f f 2 2 f f f f f . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f 2 f 2 e f . . 
+        . . f f f 2 2 e e 2 2 f f f . . 
+        . f f e f 2 f e e f 2 f e f f . 
+        . f e e f f e e e e f e e e f . 
+        . . f e e e e e e e e e e f . . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f . . . . 
+        . . 4 d f 2 2 2 2 2 2 f . . . . 
+        . . 4 4 f 4 4 4 4 4 4 f . . . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . . f f f f 2 2 f f f f . . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f f 2 f e f . . 
+        . . f f f 2 f e e 2 2 f f f . . 
+        . . f e 2 f f e e 2 f e e f . . 
+        . f f e f f e e e f e e e f f . 
+        . f f e e e e e e e e e e f f . 
+        . . . f e e e e e e e e f . . . 
+        . . . e f f f f f f f f . . . . 
+        . . . 4 f 2 2 2 2 f f f . . . . 
+        . . . e f f f f f f f f . . . . 
+        . . . . f f f . . . . . . . . . 
+        `,img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . f f f f f 2 2 f f f f f . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f 2 f 2 e f . . 
+        . . f f f 2 2 e e 2 2 f f f . . 
+        . f f e f 2 f e e f 2 f e f f . 
+        . f e e f f e e e e f e e e f . 
+        . . f e e e e e e e e e e f . . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f . . . . 
+        . . 4 d f 2 2 2 2 2 2 f . . . . 
+        . . 4 4 f 4 4 4 4 4 4 f . . . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . . f f f f 2 2 f f f f . . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e f 2 f f f 2 f 2 e f . . 
+        . . f f f 2 2 e e f 2 f f f . . 
+        . . f e e f 2 e e f f 2 e f . . 
+        . f f e e e f e e e f f e f f . 
+        . f f e e e e e e e e e e f f . 
+        . . . f e e e e e e e e f . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . . f f 2 2 2 2 2 f . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . . . . . . . f f f . . . . 
+        `],
+    500,
+    false
+    )
+})
+sprites.on_fire_created(function (location) {
+    scene.createParticleEffectAtLocation(location, effects.fire)
+    sprites.set_flame_strength(location, 5)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tree fire`, function (sprite, location) {
+    scene.cameraShake(10, 500)
+    mySprite.startEffect(effects.fire, 2000)
+    info.changeLifeBy(-1)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . f f f f f f . . . . . . 
+        . . . f 2 f e e e e f f . . . . 
+        . . f 2 2 2 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 2 2 2 2 e e f f f f . . . 
+        . f 2 e f f f f 2 2 2 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d d 4 e e e f . . . 
+        . f f f e 4 4 4 e e f f . . . . 
+        . 9 f f 2 2 2 2 2 2 f . . . . . 
+        9 f f f 2 2 2 2 2 2 f . . . . . 
+        . . . f 5 5 4 f f f f . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . f 2 f e e e e f f . . . . 
+        . . f 2 2 2 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 2 2 2 2 e e f f f f . . . 
+        . f 2 e f f f f 2 2 2 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d d 4 e e e f . . . 
+        . f f f e 4 4 4 e d d f . . . . 
+        . 9 f f 2 2 2 2 2 2 2 f . . . . 
+        . f f f 5 5 4 4 4 4 4 f . . . . 
+        . . . f f f f f f f f f . . . . 
+        . . f f f f . . . f f . . . . . 
+        `,img`
+        . . . . f f f f f f . . . . . . 
+        . . . f 2 f e e e e f f . . . . 
+        . . f 2 2 2 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 2 2 2 2 e e f f f f . . . 
+        . f 2 e f f f f 2 2 2 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d d 4 e e e f . . . 
+        . f f f e 4 4 4 e e f f . . . . 
+        . 9 f f 2 2 2 2 2 2 f . . . . . 
+        9 f f f 2 2 2 2 2 2 f . . . . . 
+        . . . f 5 5 4 f f f f . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . f 2 f e e e e f f . . . . 
+        . . f 2 2 2 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 2 2 2 2 e e f f f f . . . 
+        . f 2 e f f f f 2 2 2 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d d 4 e e e f . . . 
+        . f f f e 4 4 4 e d d f . . . . 
+        . 9 f f 2 2 2 2 2 2 2 f . . . . 
+        . f f f 5 5 4 4 4 4 4 f . . . . 
+        . . . f f f f f f f f f . . . . 
+        . . f f f f . . . f f . . . . . 
+        `,img`
+        . . . . f f f f f f . . . . . . 
+        . . . f 2 f e e e e f f . . . . 
+        . . f 2 2 2 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 2 2 2 2 e e f f f f . . . 
+        . f 2 e f f f f 2 2 2 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d d 4 e e e f . . . 
+        . f f f e 4 4 4 e e f f . . . . 
+        . 9 f f 2 2 2 2 2 2 f . . . . . 
+        9 f f f 2 2 2 2 2 2 f . . . . . 
+        . . . f 5 5 4 f f f f . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        `],
+    500,
+    false
+    )
+})
+sprites.on_fire_destroyed(function (location) {
+    scene.clearParticleEffectsAtLocation(location)
+    tiles.setTileAt(location, assets.tile`burnt tree`)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e 4 d d d d f . . . 
+        . . . . f f e e 4 4 4 e f . . . 
+        . . . . . f 2 2 2 2 2 e f f f . 
+        . . . . . f 2 2 2 2 2 e f f 9 . 
+        . . . . . f e e 4 4 e e f f f . 
+        . . . . . . f f f f f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e e e d d d f . . . 
+        . . . . f f 4 d d e 4 e f f f . 
+        . . . . . f 2 2 2 2 2 2 f f 9 . 
+        . . . . . f e 4 4 4 5 5 f f f . 
+        . . . . . f f f f f f f f . . . 
+        . . . . . f f . . . f f f f . . 
+        `,img`
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e 4 d d d d f . . . 
+        . . . . f f e e 4 4 4 e f . . . 
+        . . . . . f 2 2 2 2 2 e f f f . 
+        . . . . . f 2 2 2 2 2 e f f 9 . 
+        . . . . . f e e 4 4 e e f f f . 
+        . . . . . . f f f f f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e e e d d d f . . . 
+        . . . . f f 4 d d e 4 e f f f . 
+        . . . . . f 2 2 2 2 2 2 f f 9 . 
+        . . . . . f e 4 4 4 5 5 f f f . 
+        . . . . . f f f f f f f f . . . 
+        . . . . . f f . . . f f f f . . 
+        `,img`
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e 4 d d d d f . . . 
+        . . . . f f e e 4 4 4 e f . . . 
+        . . . . . f 2 2 2 2 2 e f f f . 
+        . . . . . f 2 2 2 2 2 e f f 9 . 
+        . . . . . f e e 4 4 e e f f f . 
+        . . . . . . f f f f f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        `],
+    500,
+    false
+    )
+})
+scene.onOverlapTile(SpriteKind.Water, assets.tile`tree fire`, function (sprite, location) {
+    sprite.destroy()
+    sprites.change_flame_strength_by(location, -1)
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f f . . 
+        . . . . f 2 2 f e f f f f e f . 
+        . . . . f 2 2 f 4 f 9 9 f 4 f . 
+        . . . . f 4 4 f 4 f f f f 4 f . 
+        . . . . . f f f f f f . . f . . 
+        . . . . . . . . . f f . . . . . 
+        `,img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f f . . 
+        . . . . f 2 2 f e f f f f e f . 
+        . . . . f 2 2 f 4 f 9 9 f 4 f . 
+        . . . . f 4 4 f 4 f f f f 4 f . 
+        . . . . . f f f f f f . . f . . 
+        . . . . . f f . . . . . . . . . 
+        `,img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f f . . 
+        . . . . f 2 2 f e f f f f e f . 
+        . . . . f 2 2 f 4 f 9 9 f 4 f . 
+        . . . . f 4 4 f 4 f f f f 4 f . 
+        . . . . . f f f f f f . . f . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f f . . 
+        . . . . f 2 2 f e f f f f e f . 
+        . . . . f 2 2 f 4 f 9 9 f 4 f . 
+        . . . . f 4 4 f 4 f f f f 4 f . 
+        . . . . . f f f f f f . . f . . 
+        . . . . . . . . . f f . . . . . 
+        `,img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f f . . 
+        . . . . f 2 2 f e f f f f e f . 
+        . . . . f 2 2 f 4 f 9 9 f 4 f . 
+        . . . . f 4 4 f 4 f f f f 4 f . 
+        . . . . . f f f f f f . . f . . 
+        . . . . . f f . . f f . . . . . 
+        `],
+    500,
+    false
+    )
+})
+controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (ammoleft > 0) {
+        sprites.spray(mySprite, assets.image`water`)
+        ammoleft += -1
+    }
+})
+info.onLifeZero(function () {
+    game.gameOver(false)
+})
+controller.A.onEvent(ControllerButtonEvent.Repeated, function () {
+    if (ammoleft) {
+        sprites.spray(mySprite, assets.image`water`)
+        ammoleft += -1
+    }
+})
+let ammoleft = 0
+let mySprite: Sprite = null
+music.play(music.stringPlayable("E B C G E B D G ", 90), music.PlaybackMode.LoopingInBackground)
+game.showLongText("A person threw junk in the forest and lit it up. It is up to you to put out the fire. Good luck. Move with WASD keys and hold space bar to shoot water. The forest's life are in your hand. Remember, you only have 30 lives. Also don't forget to spread the word about not lighting junk up in the forest.", DialogLayout.Center)
+game.set_dryness_of_grass(10)
+game.set_strength_of_wind(19)
+game.set_health_of_trees(20)
+tiles.setTilemap(tilemap`level10`)
+mySprite = sprites.create(assets.image`Fire Plane Right`, SpriteKind.Player)
+info.setLife(30)
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
+for (let index = 0; index < 4; index++) {
+    sprites.create_spreading_fire(assets.tile`tree`, assets.tile`tree fire`)
+}
+hud.fire_hud(true)
+hud.danger_hud(true)
+hud.forest_hud_healthy(7)
+hud.forest_hud_burned(2)
+ammoleft = 1000
+game.setGameOverEffect(false, effects.clouds)
+game.setGameOverEffect(true, effects.confetti)
+game.onUpdate(function () {
+    sprites.random_spread()
+})
